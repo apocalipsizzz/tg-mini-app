b692523d5741f36944a4ecf57c519feaac9dd1af
jora <jora@de.ru>
2024-09-04 17:09:00 +0300

    [fix] Tarot Wheel

--- src/components/TarotWheel/TarotWheel.tsx
+++ src/components/TarotWheel/TarotWheel.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-// import { motion, useMotionValue, useTransform } from "framer-motion";
+import React, { useState } from "react";
+import { motion, useAnimation, useMotionValue } from "framer-motion";
 import "./TarotWheel.css";
 
 const cardData = [
@@ -91,7 +91,7 @@ type Card = {
 };
 
 const width = window.innerWidth;
-// const height = window.innerHeight;
+const height = window.innerHeight;
 const numberOfCards = cardData.length;
 const _size = 80;
 const _cardSize = {
@@ -110,10 +110,75 @@ const curcleRadius = Math.max(
   width / curcleVisibilityPercentage
 );
 
+const rotateV = {
+  start: ({ rotate }: { rotate: number }) => ({
+    rotate: rotate,
+    transition: { type: "tween", duration: 8, ease: [0.5, 0.1, 0.15, 1] },
+  }),
+  stop: ({ endDeg }: { endDeg: number }) => ({
+    rotate: endDeg,
+    transition: { type: "tween", duration: 0 },
+  }),
+};
+
+export const TarotWheel = () => {
+  const [isAnimationStart, setIsAnimationStart] = useState(false);
+
+  const rotateControl = useAnimation();
+  const spinCount = 3;
+  // position will come from api, it's between 1 ... 8
+  const position = 4;
+  const offset = (360 / numberOfCards) * position;
+  const endValue = useMotionValue(360 * spinCount - offset);
+  const onAnimationStart = (info) => {
+    console.log("isAnimationStart", isAnimationStart);
+    setIsAnimationStart(true);
+    rotateControl.start("start");
+    console.log("info", info);
+    // setTimeout(() => {
+    //   rotateControl.start("stop");
+    //   pulseControl.start(vPulse);
+    //   setIsAnimationStart(false);
+    // }, 8000);
+  };
+  const onAnimationEnd = () => {
+    rotateControl.start("stop");
+    setIsAnimationStart(false);
+  };
+
+  return (
+    <>
+      <motion.div
+        className="card-wheel"
+        style={{
+          width: curcleRadius * 2,
+          height: curcleRadius * 2,
+          borderRadius: curcleRadius,
+          position: "absolute",
+          top: height - curcleRadius * 0.8,
+        }}
+        custom={{
+          rotate: [0, endValue.current],
+          endDeg: endValue.current,
+        }}
+        animate={rotateControl}
+        variants={rotateV}
+        onPanStart={onAnimationStart}
+        onClick={onAnimationEnd}
+        // onPanEnd={onAnimationEnd}
+      >
+        {cardData.map((card, index) => {
+          return <TarotCard key={card.id} card={card} index={index} />;
+        })}
+      </motion.div>
+    </>
+  );
+};
+
 const TarotCard = ({ card, index }: { card: Card; index: number }) => {
   return (
     <>
-      <div
+      <motion.div
         className="card__item"
         style={{
           width: _cardSize.width,
@@ -131,28 +196,7 @@ const TarotCard = ({ card, index }: { card: Card; index: number }) => {
           }}
           alt="Placeholder alt"
         />
-      </div>
-    </>
-  );
-};
-
-export const TarotWheel = () => {
-  return (
-    <>
-      <div
-        className="card-wheel"
-        style={{
-          width: curcleRadius * 2,
-          height: curcleRadius * 2,
-          borderRadius: curcleRadius,
-          position: "absolute",
-          bottom: -curcleRadius * 1.1,
-        }}
-      >
-        {cardData.map((card, index) => {
-          return <TarotCard key={card.id} card={card} index={index} />;
-        })}
-      </div>
+      </motion.div>
     </>
   );
 };
