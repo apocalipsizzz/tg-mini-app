921ccb74051ccf0b3f5252695d0f477e6d45df97
jora <jora@de.ru>
2024-09-05 13:06:41 +0300

    [fix] Card Swiper off

--- src/components/CardSwiper/CardSwiper.tsx
+++ src/components/CardSwiper/CardSwiper.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 import { cardData } from "../../localData/cardData";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -10,13 +11,25 @@ import "./CardSwiper.css";
 export const CardSwiper = () => {
   const initialPosition = Math.round(cardData.length / 2);
 
+  const handleSelectedSlide = (event) => {
+    if (event.currentTarget) {
+      event.currentTarget.classList.add("selected");
+    }
+  };
+
   const handleSlideChange = (swiper: {
     slides: HTMLElement[];
     activeIndex: number;
   }) => {
-    console.log("swiper", swiper);
+    console.log("swiper", swiper.activeIndex);
     const selectedSlide = swiper.slides[swiper.activeIndex];
     console.log("selectedSlide22", selectedSlide);
+    swiper.slides.forEach((slide) => {
+      slide.classList.remove("selected");
+      slide.removeEventListener("click", handleSelectedSlide);
+    });
+
+    selectedSlide.addEventListener("click", handleSelectedSlide);
   };
 
   return (
@@ -32,11 +45,13 @@ export const CardSwiper = () => {
         grabCursor={true}
         initialSlide={initialPosition}
         onSlideChange={handleSlideChange}
+        loop
+        loopAdditionalSlides={4}
       >
         {cardData.map((card) => {
           return (
             <SwiperSlide key={card.id}>
-              <img src={card.url} alt={card.id} />
+              <motion.img src={card.url} alt={card.id} />
             </SwiperSlide>
           );
         })}
