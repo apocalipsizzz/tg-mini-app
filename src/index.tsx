e4fc68a9b17ab2c3fb849b1eaf8999ce46fa7fea
jora <jora@de.ru>
2024-09-10 15:06:43 +0300

    [fix] refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Navigation } from "./components/Navigation/Navigation";
 import { TarotWheel } from "./components/TarotWheel/TarotWheel";
 // import { CardSwiper } from "./components/CardSwiper/CardSwiper";
-import "./index.css";
+import "./assets/style/style.css";
 
 createRoot(document.getElementById("root")!).render(
   <div className="wrapper">
